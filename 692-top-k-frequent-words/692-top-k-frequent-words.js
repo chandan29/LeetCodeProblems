3ff88835c78ebf32bfd6634cf908359c1c54adf8
Chandan Paranjape <chandan.paranjape@sjsu.edu>
2022-03-05 08:14:41 -0800

    Time: 94 ms (72.76%), Space: 47.2 MB (31.09%) - LeetHub

---
+++ 692-top-k-frequent-words/692-top-k-frequent-words.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string[]} words
+ * @param {number} k
+ * @return {string[]}
+ */
+var topKFrequent = function(words, k) {
+    let m = new Map();
+    words.forEach(word => m.set(word, m.get(word)+1 || 1));
+    let arr = Array.from(m.entries()).sort((a,b) => {
+					return a[1] === b[1] ? a[0].localeCompare(b[0]) : b[1]-a[1];
+					}).slice(0,k).map(pair => pair[0]);
+    return arr;
+};
