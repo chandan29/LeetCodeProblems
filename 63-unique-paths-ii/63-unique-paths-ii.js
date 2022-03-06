71138d56e15bf3810e5714e2b9ec55b10661388c
Chandan Paranjape <chandan.paranjape@sjsu.edu>
2022-03-06 04:22:58 -0800

    Time: 78 ms (75.04%), Space: 42.4 MB (52.88%) - LeetHub

---
+++ 63-unique-paths-ii/63-unique-paths-ii.js
@@ -0,0 +1,22 @@
+const dfs = (grid, row, col, dp) => {
+    if (row >= grid.length || col >= grid[0].length|| grid[row][col] ==1) {
+        return 0;
+    }
+    if (row==grid.length-1 && col == grid[0].length-1) {
+        return 1;
+    }
+    if (dp[row][col] != null) {
+        return dp[row][col]
+    }
+    dp[row][col] = dfs(grid, row+1, col, dp)+ dfs(grid, row, col+1, dp);
+    
+    return dp[row][col];
+}
+var uniquePathsWithObstacles = function(grid) {
+    const dp = new Array(grid.length);
+    for(let i=0;i< grid.length;i++) {
+        dp[i] = new Array(grid[0].length);
+    }
+    
+    return dfs(grid, 0, 0, dp);
+};
