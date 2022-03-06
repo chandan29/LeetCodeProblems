6afc6810765d391ea2919aa36184e1967f3bdce5
Chandan Paranjape <chandan.paranjape@sjsu.edu>
2022-03-06 00:47:44 -0800

    Time: 219 ms (35.14%), Space: 55.6 MB (35.42%) - LeetHub

---
+++ 173-binary-search-tree-iterator/173-binary-search-tree-iterator.js
@@ -0,0 +1,25 @@
+var BSTIterator = function(root) {
+    const inorder = [];                      //queue
+    getInorderOrder(root, inorder);
+    this.inorder = inorder;
+};
+
+BSTIterator.prototype.next = function() {
+    if(!this.inorder.length){
+        return -1;
+    }
+    return this.inorder.shift();
+};
+
+BSTIterator.prototype.hasNext = function() {
+    return this.inorder.length > 0;
+};
+
+function getInorderOrder(root, inorder) {
+    if(!root) {
+        return;
+    }
+    getInorderOrder(root.left, inorder);
+    inorder.push(root.val);
+    getInorderOrder(root.right, inorder);
+}
