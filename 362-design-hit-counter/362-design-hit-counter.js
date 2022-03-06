8e708dc138ece8506658e40b68720b4a261350e4
Chandan Paranjape <chandan.paranjape@sjsu.edu>
2022-03-05 23:37:43 -0800

    Time: 80 ms (63.54%), Space: 41.9 MB (43.92%) - LeetHub

---
+++ 362-design-hit-counter/362-design-hit-counter.js
@@ -0,0 +1,46 @@
+
+class HitCounter {
+    constructor(n = 300){
+        this.n = n;
+        this.hits = new Array(this.n).fill(0);
+        this.times = new Array(this.n).fill(0);
+        this.hit = this.hit.bind(this);
+        this.getHits = this.getHits.bind(this);
+    }
+    
+    
+    /**
+     * Record a hit.
+            @param timestamp - The current timestamp (in seconds granularity). 
+     * @param {number} timestamp
+     * @return {void}
+     */
+    hit(timestamp){
+        const index = timestamp % this.n;
+        
+        if (this.times[index] !== timestamp) {
+            // in case we are handling next (n > 1) 5 minute span
+            this.times[index] = timestamp;
+            this.hits[index] = 1;
+        } else {
+            // this is same 5 minute span
+           this.hits[index] += 1;
+        }
+    }
+    /**
+     * Return the number of hits in the past 5 minutes.
+            @param timestamp - The current timestamp (in seconds granularity). 
+     * @param {number} timestamp
+     * @return {number}
+     */
+    getHits(timestamp) {
+        let total = 0;
+        for (let i =0; i < this.n; i++) {
+            if (timestamp - this.times[i] < this.n) {
+                total += this.hits[i];
+            }
+        }
+        
+        return total;
+    }
+}
