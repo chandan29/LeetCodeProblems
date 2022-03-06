9e43dc98bb244a295a707919f0ec5425242a2dc9
Chandan Paranjape <chandan.paranjape@sjsu.edu>
2022-03-06 00:31:41 -0800

    Time: 108 ms (68.86%), Space: 45.1 MB (54.39%) - LeetHub

---
+++ 200-number-of-islands/200-number-of-islands.js
@@ -0,0 +1,38 @@
+/**
+ * @param {character[][]} grid
+ * @return {number}
+ */
+var numIslands = function(grid) {
+    var count = 0;
+    for(var i=0;i<grid.length;i++){
+        for(var j=0;j<grid[0].length;j++){
+            if(grid[i][j]==='1'){
+                count++;
+                helper(grid,i,j);
+            }
+        }
+    }
+    
+    return count;
+};
+
+
+var helper = function(grid, row, col){
+    if(row<0 || row>=grid.length){
+        return;
+    }
+    if(col<0 || col>=grid[0].length){
+        return;
+    }
+    if(grid[row][col]==='0'){
+        return;
+    }
+    grid[row][col] = '0';
+    
+    helper(grid,row+1,col);
+    helper(grid,row-1,col);
+    helper(grid,row,col+1);
+    helper(grid,row,col-1);
+    
+}
+
