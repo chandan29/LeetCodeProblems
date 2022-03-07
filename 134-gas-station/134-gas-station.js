a2cb51c76773e0247ecc19667b40886724ca5174
Chandan Paranjape <chandan.paranjape@sjsu.edu>
2022-03-07 01:01:36 -0800

    Time: 100 ms (72.20%), Space: 50.2 MB (24.68%) - LeetHub

--- 134-gas-station/134-gas-station.js
+++ 134-gas-station/134-gas-station.js
@@ -4,16 +4,23 @@
  * @return {number}
  */
 var canCompleteCircuit = function(gas, cost) {
-    let currGas = 0;
-    let station = 0;
-    let totalGas = 0;
-  for (let i = 0; i < gas.length; i++) {
-    currGas += gas[i] - cost[i];
-    totalGas += gas[i] - cost[i];
-    if (currGas < 0) {
-      currGas = 0;
-      station = i + 1; // Last station not ample, try next
+    let start = 0;
+    let current = 0;
+    let total = 0;
+    
+    for(let i=0;i<gas.length;i++){
+        total+= gas[i] - cost[i];
+        current+= gas[i] - cost[i];
+        if(current<0){
+            current = 0;
+            start = i+1;
+        }
     }
-  }
-  return totalGas < 0 ? -1 : station;
+    
+    if(total<0){
+        return -1;
+    }
+    else{
+        return start;
+    }
 };
