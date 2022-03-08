6980bfcf9f89c8e6e55d50f4a80423a924cf7543
Chandan Paranjape <chandan.paranjape@sjsu.edu>
2022-03-08 00:51:13 -0800

    Time: 177 ms (92.98%), Space: 71.5 MB (39.67%) - LeetHub

---
+++ 310-minimum-height-trees/310-minimum-height-trees.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number} n
+ * @param {number[][]} edges
+ * @return {number[]}
+ */
+var findMinHeightTrees = function(n, edges) {
+    if (n == 1) return [0];
+    
+    let adj = [];
+    
+    for (let i = 0; i < n; i++) {
+        adj[i] = new Set();
+    }
+    
+    for (let [i,j] of edges) {
+        adj[i].add(j);
+        adj[j].add(i);
+    }
+    
+    
+    let leaves = [];
+    
+    for (let i = 0; i < n; i++) {
+        if (adj[i].size == 1) {
+            leaves.push(i);
+        }
+    }
+    
+    while (n > 2) {
+        n -= leaves.length;
+        let newLeaves = [];
+        
+        for (let i of leaves) {
+            let j = Array.from(adj[i])[0];
+            adj[j].delete(i);
+            
+            if (adj[j].size == 1) {
+                newLeaves.push(j);
+            }
+        }
+        
+        leaves = newLeaves;
+    }
+    
+    return leaves;
+};
