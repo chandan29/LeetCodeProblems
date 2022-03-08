46d2abca7d6d4f521d102a2e914b02448eb2bade
Chandan Paranjape <chandan.paranjape@sjsu.edu>
2022-03-08 13:46:00 -0800

    Time: 144 ms (56.52%), Space: 52.8 MB (29.19%) - LeetHub

---
+++ 1578-minimum-time-to-make-rope-colorful/1578-minimum-time-to-make-rope-colorful.js
@@ -0,0 +1,27 @@
+/**
+ * @param {string} colors
+ * @param {number[]} neededTime
+ * @return {number}
+ */
+var minCost = function(colors, neededTime) {
+    let time = 0;
+    
+    for(let i = colors.length-1;i>=0;i--){
+        let curr = colors[i];
+        let currVal = neededTime[i];
+        let j = i-1;
+        while(colors[j]===curr && j>=0){
+            if(currVal<neededTime[j]){
+                time+=currVal;
+                currVal = neededTime[j];
+            }
+            else{
+                time+=neededTime[j];
+            }
+            j--;
+        }
+        i = j + 1;
+    }
+    
+    return time;
+};
