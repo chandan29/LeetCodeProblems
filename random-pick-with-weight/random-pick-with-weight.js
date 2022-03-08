642b271d95dbe231dd7f411990f69d21de86302c
Chandan Paranjape <chandan.paranjape@sjsu.edu>
2022-03-08 01:08:25 -0800

    Time: 136 ms, Memory: 51.2 MB - LeetHub

---
+++ random-pick-with-weight/random-pick-with-weight.js
@@ -0,0 +1,28 @@
+var Solution = function(w) {
+    this.sum = [];
+    w.forEach((el,i)=>{
+        this.sum.push( (this.sum[i-1] || 0) + el);
+    });
+    this.total = this.sum[this.sum.length-1];
+};
+
+Solution.prototype.pickIndex = function() {
+    var rand =  Math.floor( Math.random()*(this.total)+1); 
+    // binary search - lower bound
+    var l = 0; 
+    var r = this.sum.length-1;
+    while(l<r)
+    {
+        var mid = Math.floor((l+r)/2);
+        if(this.sum[mid] >= rand) 
+        {
+            r = mid;
+        }
+        else
+        {
+            l = mid+1;
+        }
+    }
+    return l;
+    
+};
