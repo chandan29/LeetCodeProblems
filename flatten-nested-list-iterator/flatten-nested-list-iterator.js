31bbc77af21572b80caec10a84239813cdfef0ab
Chandan Paranjape <chandan.paranjape@sjsu.edu>
2022-03-08 02:02:17 -0800

    Time: 92 ms, Memory: 53.7 MB - LeetHub

---
+++ flatten-nested-list-iterator/flatten-nested-list-iterator.js
@@ -0,0 +1,16 @@
+class NestedIterator{
+    constructor(nestedList) {
+        this.data = []
+        this.flatten(nestedList)
+    };
+    
+    flatten(list) {
+        for (let el of list)
+            if (el.isInteger()) this.data.push(el.getInteger())
+            else this.flatten(el.getList())
+    };
+    
+    hasNext() { return this.data.length };
+    
+    next() { return this.data.shift() };
+}
