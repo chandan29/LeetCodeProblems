41469ead34a05a9ea24e7998a7057d982cc55845
Chandan Paranjape <chandan.paranjape@sjsu.edu>
2022-03-11 02:56:42 -0800

    Time: 100 ms (56.72%), Space: 44.5 MB (48.51%) - LeetHub

---
+++ 665-non-decreasing-array/665-non-decreasing-array.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var checkPossibility = function(nums) {
+    let badIndex = -1;
+    
+    for(let i=0;i<nums.length-1;i++){
+        if(nums[i]>nums[i+1]){
+            if(badIndex===-1){
+                badIndex = i;
+            }
+            else{
+                return false;
+            }
+        }
+    }
+    if(badIndex<=0 || badIndex === nums.length-2){
+        return true;
+    }
+    else{
+        return nums[badIndex-1] <= nums[badIndex+1] || nums[badIndex]<=nums[badIndex+2]
+    }
+};
