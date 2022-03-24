ef2602aa9eed695b25986ed46375ec8259fab4ae
Chandan Paranjape <chandan.paranjape@sjsu.edu>
2022-03-24 00:33:08 -0700

    Time: 109 ms (12.30%), Space: 42.3 MB (83.98%) - LeetHub

---
+++ 339-nested-list-weight-sum/339-nested-list-weight-sum.js
@@ -0,0 +1,23 @@
+var depthSum = function(nestedList) {
+    var sum = 0;
+    return helper(nestedList,1,0);
+    
+    
+    function helper(list,d){
+        for( let i=0; i<list.length; i++ ) {
+        let val = list[i];
+        
+        if( val.isInteger() ) {
+            val = val.getInteger()
+            sum += val * d;
+        } else {
+            helper( val.getList(), d+1 );
+        }
+    }
+    return sum ;
+    }
+    
+    
+    
+    
+};
