e868319883343d5be25b6af0a9f5528b82408241
Chandan Paranjape <chandan.paranjape@sjsu.edu>
2022-03-23 17:56:43 -0700

    Time: 88 ms (88.73%), Space: 44.4 MB (74.72%) - LeetHub

--- 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.js
+++ 340-longest-substring-with-at-most-k-distinct-characters/340-longest-substring-with-at-most-k-distinct-characters.js
@@ -1,12 +1,25 @@
-function lengthOfLongestSubstringKDistinct(s, k) {
-    let longest = 0;
-    const charCount = {};
-    for (let left = 0, right = 0; right < s.length; right++) {
-        charCount[s[right]] = (charCount[s[right]] ?? 0) + 1;
-        for (; Object.keys(charCount).length > k; left++)
-            if (--charCount[s[left]] === 0)
-                delete charCount[s[left]]
-        longest = Math.max(longest, right - left + 1);
+const lengthOfLongestSubstringKDistinct = (s, k) => {
+    if (!s || !k) { return 0; }
+    if (s.length <= k) { return s.length; }
+    
+    let longest = 0, map = new Map(), l = 0, r = 0, n = s.length;
+    
+    while (r < n) {
+        while (r < n && (map.size < k || map.has(s[r]))) {
+            let occur = map.get(s[r]) || 0;
+            map.set(s[r++], ++occur);
+        }
+        
+        longest = Math.max(longest, r - l);
+        
+        let leftCh = s[l++], leftOccur = map.get(leftCh);
+        
+        if (leftOccur === 1) {
+            map.delete(leftCh);
+        } else {
+            map.set(leftCh, --leftOccur);
+        }
     }
+    
     return longest;
-}
+};
