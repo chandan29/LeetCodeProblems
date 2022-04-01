a4b17682038a9420ea5896d9a0fae48719e7ad42
Chandan Paranjape <chandan.paranjape@sjsu.edu>
2022-04-01 03:56:10 -0700

    Time: 574 ms (16.35%), Space: 63.6 MB (25.63%) - LeetHub

---
+++ 973-k-closest-points-to-origin/973-k-closest-points-to-origin.js
@@ -0,0 +1,21 @@
+var kClosest = function(points, k) {
+    let maxPQ = new MaxPriorityQueue()
+    for (let point of points) {
+        let dist = squaredDistance(point)
+        if (maxPQ.size() < k) {
+            // Fill the max PQ up to k points
+            maxPQ.enqueue(point, dist)
+        } else if (dist < maxPQ.front().priority) {
+            // If the max PQ is full and a closer point is found,
+            // discard the farthest point and add this one
+            maxPQ.dequeue()
+            maxPQ.enqueue(point, dist)
+        }
+    }
+    
+    // Return all points stored in the max PQ
+    return maxPQ.toArray().map(el => el.element)
+};
+
+// Calculate and return the squared Euclidean distance
+const squaredDistance = ([x,y]) => x ** 2 + y ** 2
